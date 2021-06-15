6fd3a6b05d70036cc15ccd4a36e1b2b25fe8d58b
ryu-g <em.ryu.g@gmail.com>
2021-06-15 21:07:40 +0900

    change canvas size

--- src/viewArea.js
+++ src/viewArea.js
@@ -1,8 +1,10 @@
 console.log("imported:viewArea.js")
 const canvas = document.getElementById('view')
 const view = canvas.getContext('2d')
-const viewWidth = canvas.width
-const viewHeight = canvas.height
+const viewWidth = window.innerWidth
+const viewHeight = window.innerHeight
+canvas.width = viewWidth
+canvas.height = viewHeight
 const line = (x1,y1,x2,y2) => {
   view.fillRect(0,255,0,100);
   view.moveTo(x1, y1)
