72bfd1c3b898ca64596a48a82891abc1d3e9c5ac
adusam <popostn@gmail.com>
2021-07-07 23:34:11 +0900

    [update] rewrite prototype to class, add prettier watch

--- src/Plane.js
+++ src/Plane.js
@@ -18,82 +18,86 @@ class Plane {
     console.log(`x : ${this.posx}`)
     console.log(`y : ${this.posy}`)
   }
-}
 
-Plane.prototype.damage = function (mount) {
-  this.health -= mount
-}
+  damege(mount) {
+    this.health -= mount
+  }
 
-Plane.prototype.heal = function () {
-  if (this.score > req_heal[this.stats.healLv]) {
-    this.score -= req_heal[this.stats.healLv]
-    this.health += 15 * this.stats.healLv
-    this.stats.healLv += 1
-  } else {
-    console.log('need more score!')
-    console.log(`get more than ${req_heal[this.stats.healLv]} points!`)
+  heal() {
+    if (this.score > req_heal[this.stats.healLv]) {
+      this.score -= req_heal[this.stats.healLv]
+      this.health += 15 * this.stats.healLv
+      this.stats.healLv += 1
+    } else {
+      console.log('need more score!')
+      console.log(`get more than ${req_heal[this.stats.healLv]} points!`)
+    }
+  }
+
+  upgradeWideShot() {
+    if (this.score > req_wideshot[this.stats.wideshotLv]) {
+      this.score -= req_wideshot[this.stats.wideshotLv]
+      this.stats.wideshotLv += 1
+    } else {
+      console.log('need more score!')
+      console.log(
+        `get more than ${req_wideshot[this.stats.wideshotLv]} points!`
+      )
+    }
+  }
+
+  upgradeRapidFire() {
+    if (this.score > req_rapidfire[this.stats.rapidFireLv]) {
+      this.score -= req_rapidfire[this.stats.rapidFireLv]
+      this.stats.rapidFireLv += 1
+    } else {
+      console.log('need more score!')
+      console.log(
+        `get more than ${req_rapidfire[this.stats.rapidFireLv]} points!`
+      )
+    }
+  }
+
+  upgradeBullet() {
+    if (this.score > req_bullet[this.stats.bulletLv]) {
+      this.score -= req_bullet[this.stats.bulletLv]
+      this.stats.bulletLv += 1
+    } else {
+      console.log('need more score!')
+      console.log(`get more than ${req_bullet[this.stats.bulletLv]} points!`)
+    }
   }
 }
 
-Plane.prototype.upgradeWideShot = function () {
-  if (this.score > req_wideshot[this.stats.wideshotLv]) {
-    this.score -= req_wideshot[this.stats.wideshotLv]
-    this.stats.wideshotLv += 1
-  } else {
-    console.log('need more score!')
-    console.log(`get more than ${req_wideshot[this.stats.wideshotLv]} points!`)
-  }
-}
-
-Plane.prototype.upgradeRapidFire = function () {
-  if (this.score > req_rapidfire[this.stats.rapidFireLv]) {
-    this.score -= req_rapidfire[this.stats.rapidFireLv]
-    this.stats.rapidFireLv += 1
-  } else {
-    console.log('need more score!')
-    console.log(
-      `get more than ${req_rapidfire[this.stats.rapidFireLv]} points!`
-    )
-  }
-}
-
-Plane.prototype.upgradeBullet = function () {
-  if (this.score > req_bullet[this.stats.bulletLv]) {
-    this.score -= req_bullet[this.stats.bulletLv]
-    this.stats.bulletLv += 1
-  } else {
-    console.log('need more score!')
-    console.log(`get more than ${req_bullet[this.stats.bulletLv]} points!`)
-  }
-}
-
-function Bullet() {
-  this.x = 0
-  this.y = 0
-  this.speed = 0
-  this.alive = false
-}
-
-Bullet.prototype.set = function (x, y, speed) {
-  this.x = x
-  this.y = y
-  this.speed = speed
-  this.alive = true
-}
-
-Bullet.prototype.move = function () {
-  this.y -= this.speed
-  if (this.y < 0 || this.x < 0 || battleFieldWidth < this.x) {
+class Bullet {
+  constructor() {
+    this.x = 0
+    this.y = 0
+    this.speed = 0
     this.alive = false
-  } else if (
-    this.y < 134 &&
-    kakashi.posx < this.x &&
-    this.x < kakashi.posx + 50
-  ) {
-    plane.score += 91
-    this.alive = false
-    kakashi.health -= 1
-    kakashi.posx = getRandomInt(0, 440)
+  }
+
+  set(x, y, speed) {
+    this.x = x
+    this.y = y
+    this.speed = speed
+    this.alive = true
+  }
+
+  move() {
+    this.y -= this.speed
+    if (this.y < 0 || this.x < 0 || battleFieldWidth < this.x) {
+      this.alive = false
+    } else if (
+      this.y < 134 &&
+      kakashi.posx < this.x &&
+      this.x < kakashi.posx + 50
+    ) {
+      plane.score += 91
+      this.alive = false
+      kakashi.health -= 1
+      kakashi.posx = getRandomInt(0, 440)
+    }
   }
 }
 
